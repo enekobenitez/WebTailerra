4593c77e528f3cf5d79a33f294b55ccab7ec03ae
Eneko Benitez <enekob02@gmail.com>
2024-05-30 11:41:01 +0200

    Lehen orrialdearen aldaketak hasita

--- src/views/mainPage/index.php
+++ src/views/mainPage/index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/7f605dc8fe.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../css/index.css">
     <title>Erronka Hasiera</title>
 </head>
 <body>
     
+    <div class="header">
+        <div class="gorakoa">
+            <img src="../../../public/Argazkiak/goierri_logo.jpg" alt="">
+            <div class="sareSozialak">
+                <i class="fa-brands fa-facebook"></i>    
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-youtube"></i>
+            </div>
+        </div>
+        <div class="berakoa">Hizkuntzak</div>
+    </div>
+    <div class="erdikoArgazkiak">
+        <img src="../../../public/Argazkiak/edificio_goierri.jpg" alt="">
+    </div>
+    <div class="berakoArgazkiak">
+        <div class="Tornoa">
+            <div class="argazkia"><img src="../../../public/Argazkiak/tornoa.png" alt=""></div>
+            <h1>Tornoa</h1>
+        </div>
+        <div class="Fresadorea">
+            <div class="argazkia"><img src="../../../public/Argazkiak/fresadora.png" alt=""></div>
+            <h1>Fresadorea</h1>
+        </div>
+        <div class="Taladroa">
+            <div class="argazkia"><img src="../../../public/Argazkiak/taladroa.png" alt=""></div>
+            <h1>Taladroa</h1>
+        </div>
+        <div class="Esmerila">
+            <div class="argazkia"><img src="../../../public/Argazkiak/esmerila.png" alt=""></div>
+            <h1>Esmerilla</h1>
+        </div>
+    </div>
 </body>
 </html>
