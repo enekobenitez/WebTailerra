1f352748e7dae91fd4ea7f1bc0d3504080eb791b
Eneko Benitez <enekob02@gmail.com>
2024-05-30 12:33:50 +0200

    aldaketa berriak informazio artxiboan

---
+++ src/views/supplier/informazioa.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/7f605dc8fe.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../css/index.css">
+    <link rel="stylesheet" href="../../css/informazioa.css">
+    <title>Tornoa</title>
+</head>
+<body>
+<?php require_once("../supplier/header.php") ?>
+   
+    <div class="osoa">
+        T-1
+        <div class="osoa1">
+            <!-- Aquí inserta el código proporcionado -->
+            <table borde>
+                <tr>
+                    <th>Makinaren izendapena</th>
+                    <td>T-12</td>
+                </tr>
+                <tr>
+                    <th>Marka</th>
+                    <td>PINACHO</td>
+                </tr>
+                <tr>
+                    <th>Modelo</th>
+                    <td>S90/200</td>
+                </tr>
+                <tr>
+                    <th>Fabrikazio Urtea</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Erosketa Urtea</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Kokapena</th>
+                    <td>107 TAILERRA</td>
+                </tr>
+                <tr>
+                    <th>Aktibo zenbakia</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>CE marka (Bai/Ez)</th>
+                    <td>BAI</td>
+                </tr>
+                <tr>
+                    <th>1215era egokiturik? (Aurrekoa ezezkoa bada)</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Oharrak</th>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+        <div class="osoa2">
+    <h1>Babeserako Ekipoak</h1>
+    <div class="osoa2textua">
+        <div class="puntoak">
+            <ol>
+                <li>Betaurrekoak</li>
+                <li>Altzairuzko punta duen segurtasun botak</li>
+                <li>Txaketa, buzoa edo bata</li>
+                <li>Eskularruak</li>
+            </ol>
+        </div>
+        <div class="argazkia">
+            <img src="../../../public/Argazkiak/señales.png" alt="">
+        </div>
+        </div>
+        </div>
+
+
+
+        <div class="osoa3">
+    <h1>Arriskuak</h1>
+    <div class="puntua">
+        <div>
+            <li>Soinean dugun zerbait, makinaren mugitzen ari den atalen batean korapilatzen: mahukak, lepokoak, hilea</li>
+            <img src="../../../public/Argazkiak/arriskua1.png" alt="">
+        </div>
+        <div>
+            <li>Piezen bizarrekin, hertz bizikin, kutxilekin... ebakiak egiteko arriskua</li>
+            <img src="../../../public/Argazkiak/arriskua2.png" alt="">
+        </div>
+        <div>
+            <li>Plateretik irteten zerbaitekin kolperen bat hartzen: pieza edo plateraren giltza</li>
+            <img src="../../../public/Argazkiak/arriskua3.png" alt="">
+        </div>
+        <div>
+            <li>Irrits egitea lurrean egon daiteken olioarekin</li>
+            <img src="../../../public/Argazkiak/arriskua4.png" alt="">
+        </div>
+    </div>
+</div>
+
+        <div class="osoa4"></div>
+        <div class="osoa5"></div>
+        <div class="osoa6"></div>
+        <div class="osoa7"></div>
+    </div>
+
+    <div class="footer">
+        <div class="argazkiaFooter"><img src="../../../public/Argazkiak/goierri_logo.jpg" alt=""></div>
+        <div class="textoFooter">
+            <p>Arromendia 2, 20240 ORDIZIA (GUIPUZKOA) <br> Tel: (+34)943 88 00 62 <br> email: bulegoa@goierrieskola.eus</p>
+        </div>
+    </div>
+</body>
+</html>
