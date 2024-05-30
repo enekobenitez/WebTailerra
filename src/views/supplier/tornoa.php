c5ff2cbd12098e210c64ae835c4e0b5f98ebf915
Eneko Benitez <enekob02@gmail.com>
2024-05-30 12:01:45 +0200

    Bigarren orrialdea bukatuta

--- src/views/supplier/tornoa.php
+++ src/views/supplier/tornoa.php
@@ -6,8 +6,7 @@
     <script src="https://kit.fontawesome.com/7f605dc8fe.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../css/index.css">
     <link rel="stylesheet" href="../../css/besteak.css">
-
-    <title>Erronka Hasiera</title>
+    <title>Tornoa</title>
 </head>
 <body>
     <div class="header">
@@ -21,22 +20,26 @@
             </div>
         </div>
     </div>
-
-        <div class="botoiak">
-            <div class="botoiaTextua"><h1>Tornoa</h1></div>
-            <button>T-1</button>
-            <button>T-2</button>
-            <button>T-3</button>
-            <button>T-4</button>
-            <button>T-5</button>
-            <button>T-6</button>
-            <button>T-7</button>
-            <button>T-8</button>
-            <button>T-9</button>
-            <button>T-10</button>
-            <button>T-11</button>
-            <button>T-12</button>
+    <div class="botoiak">
+        <div class="botoiaTextua"><h1>Tornoa:</h1></div>
+        <button>T-1</button>
+        <button>T-2</button>
+        <button>T-3</button>
+        <button>T-4</button>
+        <button>T-5</button>
+        <button>T-6</button>
+        <button>T-7</button>
+        <button>T-8</button>
+        <button>T-9</button>
+        <button>T-10</button>
+        <button>T-11</button>
+        <button>T-12</button>
+    </div>  
+    <div class="footer">
+        <div class="argazkiaFooter"><img src="../../../public/Argazkiak/goierri_logo.jpg" alt=""></div>
+        <div class="textoFooter">
+            <p>Arromendia 2, 20240 ORDIZIA (GUIPUZKOA) <br> Tel: (+34)943 88 00 62 <br> email: bulegoa@goierrieskola.eus</p>
         </div>
-       
+    </div>
 </body>
 </html>
